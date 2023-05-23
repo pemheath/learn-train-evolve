6e1e0c01b0debae16e3c3b424c2e7acad43b5645
Pemberton Heath <pem.heath@gmail.com>
2023-05-23 09:43:23 -0500

    CreateUserFrontend
    -I updated the adminDashboard.js module to handle the input and call the CreateUser endpoint.

--- web/learn-train-evolve/src/components/adminDashboard.js
+++ web/learn-train-evolve/src/components/adminDashboard.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import '../styles.css';
+import axios from 'axios';
 
 const AdminDashboard = () => {
     const[isAddingUser, setIsAddingUser] = useState(false);
@@ -17,12 +18,40 @@ const AdminDashboard = () => {
             ...prevState,
             [e.target.name]: e.target.value,
         }));
+        console.log(e.target.name);
+        console.log(e.target.value);
     };
 
-    const handleAddUser = () => {
-        // Perform logic to add the user using the userDetails state
-        // Axios client to call the lambda function
-        console.log('User details:', userDetails);
+    const handleAddUser = async () => {
+        //  logic to add the user using the userDetails, calling the /users POST endpoint
+        const userData = {
+            email: userDetails.email,
+            firstName: userDetails.firstName,
+            lastName: userDetails.lastName,
+            membership: userDetails.membership,
+            rank: userDetails.rank,
+        };
+        console.log('User data:', userData);
+        console.log('User data JSON:', JSON.stringify(userData));
+
+        try{
+            const response = await axios.post('/users', userData, {
+            });
+            if (response.ok) {
+                console.log('Success');
+                const data = await response.json();
+                console.log('Data:', data);
+                return data;
+            } else {
+                console.log('Error');
+                return response.json();
+
+            }
+
+        } catch (error) {
+            handleError(error);
+        }
+
         // Reset the form
         setUserDetails({
             email: '',
@@ -34,6 +63,22 @@ const AdminDashboard = () => {
         setIsAddingUser(false);
     };
 
+    const handleError = (error) => {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+        }
+    }
+
     return (<div>
         <h1>Administrator Dashboard</h1>
         {!isAddingUser ? (
@@ -43,7 +88,7 @@ const AdminDashboard = () => {
                 <input
                     type="email"
                     name="email"
-                    placeholder="Email"
+                    placeholder="member email"
                     value={userDetails.email}
                     onChange={handleInputChange}
                 />
@@ -61,20 +106,36 @@ const AdminDashboard = () => {
                     value={userDetails.lastName}
                     onChange={handleInputChange}
                 />
-                <input
-                    type="text"
+                <select
+                    id="membership"
                     name="membership"
-                    placeholder="Membership"
                     value={userDetails.membership}
                     onChange={handleInputChange}
-                />
-                <input
-                    type="text"
+                >
+                    <option value="">Select Membership Option</option>
+                    <option value="Unlimited">Unlimited Monthly</option>
+                    <option value="UnlimitedPlus">Unlimited Monthly Plus</option>
+                    <option value="Limited12">Limited Twelve: Twelve Classes Monthly</option>
+                    <option value="Limited6">Limited Six: Six Classes Monthly</option>
+                    <option value="PayPerClass">Drop-In</option>
+
+                </select>
+                <select
+                    id="rank"
                     name="rank"
-                    placeholder="Rank"
                     value={userDetails.rank}
                     onChange={handleInputChange}
-                />
+                >
+                    <option value="">Select Current Rank</option>
+                    <option value="White1">White 1 Stripe</option>
+                    <option value="White2">White 2 Stripes</option>
+                    <option value="White3">White 3 Stripes</option>
+                    <option value="White4">White 4 Stripes</option>
+                    <option value="Blue">Blue</option>
+                    <option value="Purple">Purple</option>
+                    <option value="Brown">Brown</option>
+                    <option value="Black">Black</option>
+                </select>
                 <button type="button" onClick={handleAddUser}>
                     Add Member
                 </button>
--- web/learn-train-evolve/src/components/headerAdmin.js
+++ web/learn-train-evolve/src/components/headerAdmin.js
@@ -3,7 +3,13 @@ import '../styles.css';
 function HeaderAdmin() {
     return <header>
         <h1>Learn Train Evolve</h1>
-        <button class = "btn-1">Sign In</button>
+        <div className="container">
+            <div className="white"></div>
+            <div className="blue"></div>
+            <div className="purple"></div>
+            <div className="brown"></div>
+            <div className="black"></div>
+        </div>
     </header>
 }
 
