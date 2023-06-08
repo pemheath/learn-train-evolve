69686bae20012a9bf76b9bdc05b5cb59273fc521
Pemberton Heath <pem.heath@gmail.com>
2023-06-07 20:32:39 -0400

    Add input form for user to input data for training session

--- web/learn-train-evolve/src/components/UserTrainingSession.js
+++ web/learn-train-evolve/src/components/UserTrainingSession.js
@@ -1,16 +1,18 @@
-import React, {useEffect} from 'react';
-import {Button, Card, ThemeProvider} from "@aws-amplify/ui-react";
+import React, {useEffect, useState} from 'react';
+import {Button, Card, SliderField, ThemeProvider} from "@aws-amplify/ui-react";
 import {useLocation} from "react-router-dom";
 import theme from "./Theme";
 
 
 
 
+
 const UserTrainingSession = () => {
-
+    // allow component to read data passed from the state of the previous component
     const location = useLocation();
-
+    // set up to format the time correctly
     const [formattedDateTime, setFormattedDateTime] = React.useState("");
+    // function for setting up the time
     useEffect(() => {
         const epochTime = location.state.userTrainingSession.timeAndDate;
         const date = new Date(epochTime * 1000); // Convert epoch time to milliseconds (*1000)
@@ -28,6 +30,12 @@ const UserTrainingSession = () => {
 
     }, [location.state.userTrainingSession.timeAndDate]);
 
+    const [showForm, setShowForm] = useState(false);
+
+    const handleClick = () => {
+        setShowForm(true);
+    };
+
     return (
         <div>
             <ThemeProvider theme = {theme} >
@@ -35,10 +43,46 @@ const UserTrainingSession = () => {
             <h3>Type: {location.state.userTrainingSession.type}</h3>
             <h3>Coach: {location.state.userTrainingSession.coach}</h3>
             <h4>{formattedDateTime}</h4>
-            <Button
-                variation="primary"
-                onClick={() => alert("Functionality pending!")}
-            >Record Training</Button>
+            <div>
+                {!showForm && (
+                    <Button
+                        variation="primary"
+                        onClick={() => handleClick()}
+                    >Record Training</Button>
+                )}
+                {showForm && (
+                    <form>
+                        <SliderField
+                            label="How did you enjoy the technique?"
+                            type="techniqueEnjoyment"
+                            min={1}
+                            max={7}
+                            step={1}
+                            defaultValue={4}
+                        />
+
+                        <SliderField
+                            label="How did you perform today?"
+                            type="performanceRating"
+                            min={1}
+                            max={7}
+                            step={1}
+                            defaultValue={4}
+                        />
+                        <SliderField
+                            label="How intense was your training?"
+                            type="techniqueEnjoyment"
+                            min={1}
+                            max={10}
+                            defaultValue={5}
+                        />
+                        );
+                        {/* Additional form fields go here */}
+
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+            </div>
     </Card>
                 </ThemeProvider>
         </div>
