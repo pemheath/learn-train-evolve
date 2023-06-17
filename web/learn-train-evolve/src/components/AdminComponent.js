953d940b02e67521f29f2b32be93cdd26352b143
Pemberton Heath <pem.heath@gmail.com>
2023-06-17 15:24:32 -0400

    Remove page content from login screen. Add alert for successful calendar sync so that message does not stick around. Add loading status for sync button.

--- web/learn-train-evolve/src/components/AdminComponent.js
+++ web/learn-train-evolve/src/components/AdminComponent.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, Card, View, Text} from "@aws-amplify/ui-react";
 import axios from "axios";
 import App from "../App";
+import {Auth} from "aws-amplify";
 
 
 
 const AdminComponent = ({cognitoUser}) => {
     const[message, setMessage]= React.useState('');
-    const[disabled, setDisabled] = React.useState(false);
+    const[loading, setLoading] = React.useState(false);
+    const[groups, setGroups] = React.useState([]);
+
+    const [admin, setAdmin] = React.useState(false);
+
+    useEffect(() => {
+        async function fetchUserGroups() {
+            try {
+                console.log("calling fetch usergroups");
+                const groups = cognitoUser.signInUserSession.accessToken.payload['cognito:groups'];
+                console.log("groups", groups);
+                setGroups(groups);
+                if (groups.includes('admin')) {
+                    console.log("user is admin");
+                    setAdmin(true);
+                }
+
+            } catch (error) {
+                console.log('Error fetching user groups:', error);
+            }
+        }
+
+        fetchUserGroups(); // Call the function to execute it
+
+    }, []); // Empty dependency array for running the effect once
 
 
     async function handleClick() {
+        setLoading(true);
         console.log("in handleClick");
         console.log(cognitoUser);
         const calId = cognitoUser.attributes['custom:calId'];
@@ -27,9 +53,11 @@ const AdminComponent = ({cognitoUser}) => {
                 const response = await api.post(`training-sessions?cal=${calId}`);
                 setMessage(response.data.message);
                 console.log(message);
-                setDisabled(true);
+                setLoading(false);
+                alert(message);
 
             } catch (error) {
+                setLoading(false);
                 console.log('Error syncing sessions with user calendar ID:', error);
             }
         }
@@ -40,17 +68,16 @@ const AdminComponent = ({cognitoUser}) => {
     return(
 
         <div>
+            {admin &&
             <View>
                 <Card>
                     <Button
                         onClick={handleClick}
-                        isDisabled={disabled}
+                        isLoading={loading}
                     >Sync Training Sessions</Button>
-                    <Text>
-                        {message}
-                    </Text>
                 </Card>
             </View>
+            }
 
         </div>
         );
